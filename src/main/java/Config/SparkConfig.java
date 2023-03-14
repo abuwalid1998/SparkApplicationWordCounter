bca7fcfd7b5f107ab1ad3bf559f735b3471acb68
Amjad Khaliliah <amjadkhaliliah1998@gmail.com>
2023-03-14 08:55:59 +0200

    Add Method

--- src/main/java/Config/SparkConfig.java
+++ src/main/java/Config/SparkConfig.java
@@ -38,4 +38,24 @@ public class SparkConfig {
         }
     }
 
+    public  void wordcountwithfulldetiles(String filename,String Outputfolder,String address,String appname) {
+        try {
+            SparkConf sparkConf = new SparkConf().setMaster(address)
+                    .setAppName(appname)
+                    .set("spark.shuffle.service.enabled", "false")
+                    .set("spark.dynamicAllocation.enabled", "false");
+            JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
+            JavaRDD<String> inFile = javaSparkContext.textFile(filename);
+            JavaRDD<String> wordsfile = inFile.flatMap(content-> Arrays.asList(content.split(" ")).iterator());
+            JavaPairRDD countdata = wordsfile.mapToPair(t-> new Tuple2<>(t,1)).reduceByKey((x, y) ->(int) x + (int) y );
+
+            System.out.println("Error1");
+            countdata.saveAsTextFile(Outputfolder);
+
+        }catch (Exception e){
+            System.out.println("Error2");
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
