d1774fa1f54f8915efece70527bc56c1b354d4a2
Amjad Khaliliah <amjadkhaliliah1998@gmail.com>
2023-03-14 11:00:02 +0200

    Calculate Pi added

--- src/main/java/Config/SparkConfig.java
+++ src/main/java/Config/SparkConfig.java
@@ -1,18 +1,18 @@
 package Config;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
-
-
+import java.util.List;
 
 
 @AllArgsConstructor
@@ -44,7 +44,6 @@ public class SparkConfig {
             System.out.println(e.getMessage());
         }
     }
-
     public  void wordcountwithfulldetiles(String filename,String Outputfolder,String address,String appname) {
         try {
             SparkConf sparkConf = new SparkConf().setMaster(address)
@@ -63,6 +62,44 @@ public class SparkConfig {
             System.out.println("Error2");
             System.out.println(e.getMessage());
         }
+    }
+
+    public void CalculatePi(int slices){
+
+        SparkConf sparkConf = new SparkConf()
+                .setMaster("local")
+                .setAppName("PI Calculator").set("spark.shuffle.service.enabled", "false")
+                .set("spark.dynamicAllocation.enabled", "false");
+        final JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+        //final int slices = 1000;
+        final int n = 100000 * slices;
+        final List<Integer> l = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            l.add(i);
+        }
+        final JavaRDD<Integer> dataSet = jsc.parallelize(l, slices);
+
+
+
+        final int count = dataSet.map(integer -> {
+            double x = Math.random() * 2 - 1;
+            double y = Math.random() * 2 - 1;
+            return (x * x + y * y < 1) ? 1 : 0;
+        }).reduce((a, b) -> a + b);
+
+      String pi =""+4.0 * count / n;
+
+        try {
+            String content = pi;
+            String path = "Output/PI.text";
+            Files.write( Paths.get(path), content.getBytes());
+
+        }catch (Exception e){
+            System.out.println("Pi is roughly " + 4.0 * count / n);
+        }
+
+
+
     }
 
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,38 +1,50 @@
-
 import Config.SparkConfig;
-import org.apache.spark.*;
-import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.*;
 
 import javax.swing.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class  ScalafirstPorject {
 
 
     public static void main(String args[])  //static method
     {
-        SparkConfig sparkConfig = new SparkConfig();
 
-        JFileChooser chooser= new JFileChooser();
+        //Start Word Counter for this
 
-        //inputfile
-        int choice = chooser.showOpenDialog(null);
-        if (choice != JFileChooser.APPROVE_OPTION) return;
-        String inputfile = chooser.getSelectedFile().getPath();
+       SparkConfig sparkConfig = new SparkConfig();
+//
+//        JFileChooser chooser= new JFileChooser();
+//
+//        //inputfile
+//        int choice = chooser.showOpenDialog(null);
+//        if (choice != JFileChooser.APPROVE_OPTION) return;
+//        String inputfile = chooser.getSelectedFile().getPath();
+//
+//
+//        //outputfolder
+//         choice = chooser.showSaveDialog(null);
+//         if (choice != JFileChooser.APPROVE_OPTION) return;
+//         String outputfile = chooser.getSelectedFile().getPath();
+//
+//
+//         System.out.println(inputfile);
+//         System.out.println(outputfile);
+//
+//         sparkConfig.wordCount(inputfile,outputfile);
 
+       String slices =  JOptionPane.showInputDialog("Enter Number of slices less than 1000");
 
-        //outputfolder
-         choice = chooser.showSaveDialog(null);
-         if (choice != JFileChooser.APPROVE_OPTION) return;
-         String outputfile = chooser.getSelectedFile().getPath();
+       int num = Integer.parseInt(slices);
 
+       if (num > 1000){
+           num = 999;
+       }
 
-         System.out.println(inputfile);
-         System.out.println(outputfile);
+       sparkConfig.CalculatePi(num);
 
-         sparkConfig.wordCount(inputfile,outputfile);
     }
 }
